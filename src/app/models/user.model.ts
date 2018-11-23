4eff053186d886b968f25f3cd871e092bfccbc03
Jannou <jannou59@gmail.com>
2018-11-23 14:27:19 +0100

    feat(auth): create user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+  constructor(
+    public id?: number,
+    public createdAt?: Date,
+    public updatedAt?: Date,
+    public email?: string,
+    public firstName?: string,
+    public lastName?: string
+  ) {
+  }
+}
+
+
